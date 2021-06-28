c22d242803895f6a5c816dbff01fffd1cefc1bc2
Fernando Callejon <fcallejon@gmail.com>
2021-06-28 22:48:21 +0100

    Initial

---
+++ src/Fes.Sample/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open Fes.Domain
+
+[<EntryPoint>]
+let main _ =
+    let mapping =
+        [|
+            { MappingDefinition.Name = "field1"
+              Type = FieldTypes.Text |> Fields.Text
+              Mappings = [| Mapping.FieldMapping.Analyzer "analyzer_name" |] }
+        |]
+    let req =
+      { IndexRequest.Name = "index_test"
+        Mappings = Some mapping
+        Settings = Some { IndexSettings.NumberOfReplicas = Some 1us
+                          NumberOfShards = Some 3us }
+        Aliases = None
+        Parameters = None }
+    let t =
+        Fes.Http.fromBaseUri <| Uri "http://localhost:9200"
+        |> Fes.Http.run (IndexRequest.ToRequest req)
+        |> Async.RunSynchronously
+    printfn $"%A{t}"
+    
+    0
---
+++ src/Fes/Http.fs
@@ -0,0 +1,68 @@
+﻿namespace Fes
+
+open System
+open System.Net.Http
+open System.Text
+open FSharpPlus
+open Fleece.SystemTextJson
+
+[<RequireQualifiedAccess>]
+module Http =    
+    type RequestMsg = HttpRequestMessage
+    type ResponseMsg = HttpResponseMessage
+    
+    type Method =
+    | Get
+    | Post
+    | Put
+    | Patch
+    | Delete
+    | Head
+    | Options
+    | Trace
+    with
+        static member toNetMethod = function
+            | Get -> HttpMethod.Get
+            | Post -> HttpMethod.Post
+            | Put -> HttpMethod.Put
+            | Patch -> HttpMethod.Patch
+            | Delete -> HttpMethod.Delete
+            | Head -> HttpMethod.Head
+            | Options -> HttpMethod.Options
+            | Trace -> HttpMethod.Trace
+    
+    
+    let mkClient =
+      let client = new HttpClient()
+      client
+      
+    let fromBaseUri uri =
+      let client = mkClient
+      client.BaseAddress <- uri
+      client
+      
+    let run req (client: HttpClient) =
+        client.SendAsync req
+        |> Async.AwaitTask
+        |> Async.Catch
+        |> Async.map Result.ofChoice
+    
+    [<AutoOpen>]
+    module Request =
+        let mk (path: string) =
+            let request = new RequestMsg()
+            request.RequestUri <- Uri(path, UriKind.Relative)
+            request
+
+        let inline withFullUri uri (request: RequestMsg) =
+            request.RequestUri <- uri
+            request
+            
+        let inline withMethod method (request: RequestMsg) =
+            request.Method <- Method.toNetMethod method
+            request
+          
+        let inline withJsonBody body (request: RequestMsg) =
+            let json = toJson body |> string
+            request.Content <- new StringContent(json, Encoding.UTF8, "application/json")
+            request
