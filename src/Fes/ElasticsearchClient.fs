5c5f4f4df232e556d65c5b78fff4b4105717a714
Fernando Callejon <fcallejon@gmail.com>
2021-07-04 01:23:44 +0100

    Add ESClient instead of Http module

---
+++ src/Fes/ElasticsearchClient.fs
@@ -0,0 +1,14 @@
+﻿namespace Fes
+
+open System
+open System.Net.Http
+open Fes.Contracts.Indices
+
+type ElasticsearchClient (baseUri: Uri) =
+    let localClient =
+        let client = new HttpClient()
+        client.BaseAddress <- baseUri
+        client
+        
+    member this.mkIndex (index: IndexRequest) : AsyncResult<IndexCreateResponse, exn> =
+        Http.run index localClient
