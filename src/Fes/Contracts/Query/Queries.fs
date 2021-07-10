5fd59a35e2a28fc8fea85adc1e8362bcf01db0f4
Fernando Callejon <fcallejon@gmail.com>
2021-07-10 19:51:49 +0100

    Query contracts

---
+++ src/Fes/Contracts/Query/Queries.fs
@@ -0,0 +1,41 @@
+﻿namespace Fes.Contracts.Queries
+
+open Fleece.SystemTextJson
+
+type Rewrite =
+| ConstantScore
+| ConstantScoreBoolean
+| ScoringBoolean
+| TopTermsBlendedFreqsN
+| TopTermsBoostN
+| TopTermsN
+with
+    static member ToJson rewrite =
+        match rewrite with
+        | ConstantScore -> "constant_score"
+        | ConstantScoreBoolean -> "constant_score_boolean"
+        | ScoringBoolean -> "scoring_boolean"
+        | TopTermsBlendedFreqsN -> "top_terms_blended_freqs_N"
+        | TopTermsBoostN -> "top_terms_boost_N"
+        | TopTermsN -> "top_terms_N"
+        |> JString
+
+type BooleanLogicOperator =
+| And
+| Or
+with
+    static member ToJson op =
+        match op with
+        | And -> "AND"
+        | Or -> "OR"
+        |> JString
+        
+type QueryOperator =
+| And
+| Or
+with
+    static member ToJson op =
+        match op with
+        | And -> "and"
+        | Or -> "or"
+        |> JString
--- src/Fes/Contracts/Query/TermQueries.fs
+++ src/Fes/Contracts/Query/TermQueries.fs
@@ -1,26 +1,9 @@
 ﻿namespace Fes.Contracts.Query.TermLevel
 
 open System
+open Fes.Contracts.Queries
 open Fleece.SystemTextJson
 open Fleece.SystemTextJson.Operators
-
-type Rewrite =
-| ConstantScore
-| ConstantScoreBoolean
-| ScoringBoolean
-| TopTermsBlendedFreqsN
-| TopTermsBoostN
-| TopTermsN
-with
-    static member ToJson rewrite =
-        match rewrite with
-        | ConstantScore -> "constant_score"
-        | ConstantScoreBoolean -> "constant_score_boolean"
-        | ScoringBoolean -> "scoring_boolean"
-        | TopTermsBlendedFreqsN -> "top_terms_blended_freqs_N"
-        | TopTermsBoostN -> "top_terms_boost_N"
-        | TopTermsN -> "top_terms_N"
-        |> JString
 
 type Exists = { Field: string }
 with
